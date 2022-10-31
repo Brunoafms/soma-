6164504aa78bc1ad805d1075a3dba529da361865
Bruno de Almeida Ferreira Mendes <109405514+Brunoafms@users.noreply.github.com>
2022-10-31 03:32:11 -0300

    Add files via upload
    Problema "soma"
    Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela o valor da soma destes
    números.

---
+++ Problema soma/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+int num1,num2,num3;
+    printf("valor de x: ");
+    scanf("%d",&num1);
+    printf("valor de y: ");
+    scanf("%d",&num2);
+    num3=num1+num2;
+    printf("valor somado de x e y: %d",num3);
+    return 0;
+}
